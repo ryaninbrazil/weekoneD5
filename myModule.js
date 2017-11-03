fccd5f9a3251c7a42ff7c6b45a6f2296c9944713
Ryan <ryaninbrazil@gmail.com>
2017-11-03 22:10:07 +0000

    Require Export functions exercise

---
+++ myModule.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Number: 1,
+  mathIsFun: function() {
+    console.log("The number you see here is: " + this.Number);
+  }
+};
---
+++ myOtherModule.js
@@ -0,0 +1,5 @@
+var averageMath = require("./myModule");
+
+console.log(averageMath.Number);  // log a Number
+
+averageMath.mathIsFun();
